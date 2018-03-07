b80a0190197dc0ea0807089cd603f9822d263bf4
IFNET-FOR\ricardo.meira <ricardo.meira@ifactory.com.br>
2018-03-07 16:46:49 -0300

    adding babel to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,23 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ["es2015"]
+            }
+          }
+        }
+      ]
+  },
+  stats: {
+      colors: true
+  },
+  devtool: 'source-map'
 };
